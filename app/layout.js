a9ddc4b8176f08d8dd4388a3d85019c2998034c3
rafi9376 <rafi.shomvob9@gmail.com>
2026-05-18 02:20:25 +0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -9,9 +9,15 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <script src="https://quge5.com/88/tag.min.js" data-zone="240340" async data-cfasync="false"></script>
-      </head>
-      <body>{children}</body>
+        <head>
+  <script src="https://quge5.com/88/tag.min.js" data-zone="240340" async data-cfasync="false"></script>
+  <script src="https://pl29479097.effectivecpmnetwork.com/52/7c/8b/527c8b5a2e1f5e83eccab34fae29afbf.js"></script>
+  <script async="async" data-cfasync="false" src="https://pl29479098.effectivecpmnetwork.com/bca10afe5d05f34fa7540d30511bac31/invoke.js"></script>
+</head>
+      <body>
+  <div id="container-bca10afe5d05f34fa7540d30511bac31"></div>
+  {children}
+</body>
     </html>
   );
 }
