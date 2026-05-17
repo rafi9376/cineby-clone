3a9a0fce929802769cc7677bd9b07d3328228bd1
rafi9376 <rafi.shomvob9@gmail.com>
2026-05-18 02:01:50 +0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -8,6 +8,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script src="https://quge5.com/88/tag.min.js" data-zone="240340" async data-cfasync="false"></script>
+      </head>
       <body>{children}</body>
     </html>
   );
