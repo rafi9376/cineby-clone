1894abce457d5f5e076de77107975933e38f5330
rafi9376 <rafi.shomvob9@gmail.com>
2026-05-22 18:30:02 +0600

    Update page.js

--- app/movies/page.js
+++ app/movies/page.js
@@ -1,5 +1,5 @@
 import Navbar from '../../components/Navbar';
-import Carousel from '../../components/Carousel';
+import MovieCard from '../../components/MovieCard';
 import { getPopularMovies, getTopRatedMovies, fetchTMDB } from '../../lib/tmdb';
 
 export default async function MoviesPage() {
@@ -11,15 +11,21 @@ export default async function MoviesPage() {
   ]);
 
   const tag = m => ({ ...m, media_type: 'movie' });
+  const allMovies = [
+    ...popular.results.map(tag),
+    ...topRated.results.map(tag),
+    ...action.results.map(tag),
+    ...comedy.results.map(tag),
+  ].filter((m, i, arr) => arr.findIndex(x => x.id === m.id) === i);
 
   return (
     <>
       <Navbar />
-      <div style={{ paddingTop: 80 }}>
-        <Carousel title="Popular Movies" items={popular.results.map(tag)} />
-        <Carousel title="Top Rated" items={topRated.results.map(tag)} />
-        <Carousel title="Action" items={action.results.map(tag)} />
-        <Carousel title="Comedy" items={comedy.results.map(tag)} />
+      <div style={{ paddingTop: 80, padding: '80px 40px' }}>
+        <h1 style={{ marginBottom: 30, fontSize: 28 }}>All Movies</h1>
+        <div className="results-grid">
+          {allMovies.map(item => <MovieCard key={item.id} item={item} />)}
+        </div>
       </div>
       <footer>
         <p>Made with ❤️ using <span>TMDB API</span> • For personal use only</p>
