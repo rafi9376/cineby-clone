9c93856f4f7883037e6894a4c5ce23d113e0c2ae
rafi9376 <rafi.shomvob9@gmail.com>
2026-05-23 19:56:11 +0600

    Update page.js

--- app/bangla/natok/page.js
+++ app/bangla/natok/page.js
@@ -1,6 +1,6 @@
-import Navbar from '../../components/Navbar';
-import Carousel from '../../components/Carousel';
-import { fetchTMDB } from '../../lib/tmdb';
+import Navbar from '@/components/Navbar';
+import Carousel from '@/components/Carousel';
+import { fetchTMDB } from '@/lib/tmdb';
 
 export default async function BanglaNatokPage() {
   const [latest, popular, topRated, drama, comedy] = await Promise.all([
