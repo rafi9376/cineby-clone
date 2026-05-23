0c33a1ea9fbcfee744e39e079324e0b338c1ec4a
rafi9376 <rafi.shomvob9@gmail.com>
2026-05-24 00:20:48 +0600

    Update page.js

--- app/home/page.js
+++ app/home/page.js
@@ -1,6 +1,6 @@
 import Navbar from '@/components/Navbar';
 import Carousel from '@/components/Carousel';
-import { getTrending, getPopularMovies, getTopRatedMovies, getPopularTV, IMG_ORIGINAL } from '@/lib/tmdb';
+import { getTrending, getPopularMovies, getTopRatedMovies, getPopularTV, getNowPlaying, IMG_ORIGINAL } from '@/lib/tmdb';
 import Link from 'next/link';
 
 export default async function HomePage() {
