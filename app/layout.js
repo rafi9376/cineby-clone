e49a53ad23d78571d5200e7b362c14b535ded254
rafi9376 <rafi.shomvob9@gmail.com>
2026-05-24 20:17:59 +0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,22 +1,27 @@
+'use client';
 import './globals.css';
-
-export const metadata = {
-  title: 'Hindi & Eng - Watch Free Movies & TV Shows',
-  description: 'Watch the latest movies and TV shows online for free.',
-};
+import { usePathname } from 'next/navigation';
+import Script from 'next/script';
 
 export default function RootLayout({ children }) {
+  const pathname = usePathname();
+  const isLanding = pathname === '/';
+
   return (
-   <html lang="en">
+    <html lang="en">
       <head>
-        <script src="https://quge5.com/88/tag.min.js" data-zone="240340" async data-cfasync="false"></script>
-        <script src="https://pl29479097.effectivecpmnetwork.com/52/7c/8b/527c8b5a2e1f5e83eccab34fae29afbf.js"></script>
-        <script async="async" data-cfasync="false" src="https://pl29479098.effectivecpmnetwork.com/bca10afe5d05f34fa7540d30511bac31/invoke.js"></script>
+        {!isLanding && (
+          <>
+            <Script src="https://quge5.com/88/tag.min.js" data-zone="240340" async data-cfasync="false" />
+            <Script src="https://pl29479097.effectivecpmnetwork.com/52/7c/8b/527c8b5a2e1f5e83eccab34fae29afbf.js" />
+            <Script async data-cfasync="false" src="https://pl29479098.effectivecpmnetwork.com/bca10afe5d05f34fa7540d30511bac31/invoke.js" />
+          </>
+        )}
       </head>
       <body>
-  <div id="container-bca10afe5d05f34fa7540d30511bac31"></div>
-  {children}
-</body>
+        {!isLanding && <div id="container-bca10afe5d05f34fa7540d30511bac31"></div>}
+        {children}
+      </body>
     </html>
   );
 }
