7763fb77d4a3452443a1774bc51a4cdb943ea685
rafi9376 <rafi.shomvob9@gmail.com>
2026-05-30 23:36:38 +0600

    Create layout.js

---
+++ app/sports/layout.js
@@ -0,0 +1,20 @@
+export const metadata = {
+  title: 'Live Sports - Cricket, Football & FIFA World Cup 2026',
+  description: 'Watch live cricket, football and FIFA World Cup 2026 free.',
+  openGraph: {
+    title: 'Live Sports - Watch Cricket, Football & World Cup Free',
+    description: 'Watch live cricket, football and FIFA World Cup 2026 free.',
+    url: 'https://www.hindimoviestream.xyz/sports',
+    siteName: 'HindiMovieStream',
+    images: [{ url: 'https://www.hindimoviestream.xyz/og-image.png', width: 1200, height: 630 }],
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    images: ['https://www.hindimoviestream.xyz/og-image.png'],
+  },
+};
+
+export default function SportsLayout({ children }) {
+  return children;
+}
