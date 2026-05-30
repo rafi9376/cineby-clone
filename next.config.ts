99551c713968ad9178eb11e5757f05e4cc66ec27
rafi9376 <rafi.shomvob9@gmail.com>
2026-05-30 23:27:55 +0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: 'all',
+          },
+        ],
+      },
+    ];
+  },
 };
-
 export default nextConfig;
